f0e40eff3ee4e941672c827d4da17fde638b0dd2
“Barinder <barileo@gmail.com>
2026-05-16 07:37:40 -0700

    structure output

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,11 +27,21 @@ from langchain_tavily import TavilySearch
 #     # return "Seattle weather is sunny"
 #     return tavily.search(query=query)
 
+class Source(BaseModel):
+    """Schema for source used by the agent"""
+    url:str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answers and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+
 
 llm= ChatOpenAI(model="gpt-5")
 # tools =[search]
 tools =[TavilySearch()]
-agent=create_agent(model=llm,tools=tools)
+agent=create_agent(model=llm,tools=tools, response_format=AgentResponse)
 
 
 def main():
