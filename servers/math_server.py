7b69b580316dce15cd7ef78740ac70447116cf5f
“Barinder <barileo@gmail.com>
2026-06-05 06:33:50 -0700

    servers

---
+++ servers/math_server.py
@@ -0,0 +1,18 @@
+# __init__.py : results in creating a package in Python
+
+from fastmcp import FastMCP
+
+mcp = FastMCP("Math")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
